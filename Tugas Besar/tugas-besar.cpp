eea5b13fc0ae331bbe027f765d3866187634cb0e
Muhammad Firman Yassin <47693468+firex62@users.noreply.github.com>
2022-12-28 03:46:45 +0800

    WIP
    Program masih dalam tahap penyelesaian.

--- Tugas Besar/tugas-besar.cpp
+++ Tugas Besar/tugas-besar.cpp
@@ -1,9 +1,36 @@
 /*	Tugas Besar pemrograman komputer
-	Kelompok 1 : "Mendata dengan output file .csv"
+	Kelompok 1 : "Mendata mahasiswa dengan output file .csv"
 */
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <cstdlib>
+using namespace std;
+
+#define Namafile "Data MHS.csv"
+
+ifstream FileDataIn;
+ofstream FileDataOut;
+
+void filebaru(){
+	FileDataOut.open(Namafile);
+	FileDataOut << "\"Nama\",\"NIM\",\"Semester\",\"Jurusan\",\"Prodi\"";
+}
+
+void cekfile(){
+	FileDataIn.open(Namafile);
+	if(!FileDataIn){
+		cout << "File tidak ada! Buat file baru? (y/n) : " ;
+		char confirm ;
+		cin >> confirm ;
+		if(confirm == 'y'){
+			filebaru();
+		}
+	}
+}
 
 int main(){
+	cekfile();
+	cout << "selesai!" << endl ;
 	return 0;
 }
